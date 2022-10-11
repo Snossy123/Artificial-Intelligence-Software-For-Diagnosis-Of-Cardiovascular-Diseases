e3470951ed59f47ff04920156b34a68417c2c5a6
Snossy123 <soliemansnossy@gmail.com>
2022-10-11 22:41:36 +0200

    Build Structure of Project usingcookiecutter-data-science

---
+++ Artificial-Intelligence-Software-For-Diagnosis-Of-Cardiovascular-Diseases/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Graduation Project of faculty of Computer Science at Ain Shams University 2023',
+    author='Solieman Snossy',
+    license='',
+)
